a4c051d7ae54440d6269bc0d411a6dc4f238b8b9
y-Blandin-Luc <84115467+y-Blandin-Luc@users.noreply.github.com>
2021-06-10 14:18:18 +0900

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require("path")
 
 module.exports = {
   entry: {
@@ -8,7 +8,6 @@ module.exports = {
     ],
   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "../public"),
-  },
-};
+      filename: "main.js",
+    path: path.resolve(__dirname, "../public") }
+    };
